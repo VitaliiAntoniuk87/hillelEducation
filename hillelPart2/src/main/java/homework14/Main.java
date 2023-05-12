3134623e03dbc2c530d28fe71a64c1a89e4fc9ba
AntoniukVitalii <antoniuk.vitalii@gmail.com>
2023-05-12 21:45:56 +0300

    second commit homework14

--- hillelPart2/src/main/java/homework14/Main.java
+++ hillelPart2/src/main/java/homework14/Main.java
@@ -8,27 +8,22 @@ public class Main {
 
     public static double calculator(String firstArgument, String operator, String secondArgument) {
         double result = 0;
-        double firstOperand = 0;
-        double secondOperand = 0;
         if (operator.equals(":") && secondArgument.equals("0")) {
             System.err.println("can't divide by zero");
         } else {
             try {
-                firstOperand = Double.parseDouble(firstArgument);
-                secondOperand = Double.parseDouble(secondArgument);
+                switch (operator) {
+                    case "+" -> result = Double.parseDouble(firstArgument) + Double.parseDouble(secondArgument);
+                    case "-" -> result = Double.parseDouble(firstArgument) - Double.parseDouble(secondArgument);
+                    case ":" -> result = Double.parseDouble(firstArgument) / Double.parseDouble(secondArgument);
+                    case "x" -> result = Double.parseDouble(firstArgument) * Double.parseDouble(secondArgument);
+                }
             } catch (NumberFormatException e) {
                 System.err.println("String should contains float, double or integer");
             } catch (NullPointerException e) {
                 System.err.println("Argument can't be empty");
             }
-            switch (operator) {
-                case "+" -> result = firstOperand + secondOperand;
-                case "-" -> result = firstOperand - secondOperand;
-                case ":" -> result = firstOperand / secondOperand;
-                case "x" -> result = firstOperand * secondOperand;
-            }
         }
         return result;
     }
-
 }
